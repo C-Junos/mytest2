ab6b9651aaf723fc01bd2fe3f06ba190bdfe95c4
Dory <649087509@qq.com>
2018-10-20 11:12:31 +0800

    添加注解

--- src/main/java/com/itheima/dao/CustomerDao.java
+++ src/main/java/com/itheima/dao/CustomerDao.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 /**
  * 添加注解
+ * 再次添加注解
  */
 public interface CustomerDao extends JpaRepository<Customer,Long>,JpaSpecificationExecutor<Customer> {
 
